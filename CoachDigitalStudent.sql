e3075bca0729a5fadcb2d02b475035b4eb58325d
Ryan L <codelinde@gmail.com>
2020-02-14 09:08:36 -0700

    Fixing formatting

--- CoachDigitalStudent.sql
+++ CoachDigitalStudent.sql
@@ -6,28 +6,28 @@ SELECT DISTINCT
     'A' AS 'Action A/U (R)',
     ----School PID-----
     CASE
-		--School 1--
-		WHEN sch.schoolID = SCHOOLID1
-		THEN SCHOOLPID1
-		--School 2--
-		WHEN sch.schoolID = SCHOOLID2
-		THEN SCHOOLPID2
-	END AS 'School PID (R)',
+	--School 1--
+	WHEN sch.schoolID = SCHOOLID1
+	THEN SCHOOLPID1
+	--School 2--
+	WHEN sch.schoolID = SCHOOLID2
+	THEN SCHOOLPID2
+    END AS 'School PID (R)',
     ---------------------
     --Example username format
-    LOWER(LEFT(vahs.firstName, 1)) + LOWER(vahs.lastName) + vahs.studentNumber AS 'Username (R)',
+    LOWER(LEFT(vahs.firstName, 1)) + LOWER(vahs.lastName) AS 'Username (R)',
     'DefaultPassword' AS 'Password (R)',
     vahs.firstname AS FirstName,
     ISNULL(LEFT(vahs.middleName,1),'') AS 'MI',
     vahs.lastname AS 'Last Name',
     CASE
-		WHEN vahs.Grade LIKE 'KG'
-		THEN 'Grade K'
-		ELSE 'Grade' + ' ' + SUBSTRING(vahs.Grade, PATINDEX('%[^0]%', vahs.Grade+'.'), LEN(vahs.Grade))
-	END AS 'Grade',
+	WHEN vahs.Grade LIKE 'KG'
+	THEN 'Grade K'
+	ELSE 'Grade' + ' ' + SUBSTRING(vahs.Grade, PATINDEX('%[^0]%', vahs.Grade+'.'), LEN(vahs.Grade))
+    END AS 'Grade',
     vahs.studentNumber AS 'Student ID',
     --Example email format
-    LOWER(LEFT(vahs.firstName, 1) + vahs.lastName + vahs.studentNumber + '@mydomain.org') AS 'email address',
+    LOWER(LEFT(vahs.firstName, 1) + vahs.lastName + '@mydomain.org') AS 'email address',
     'Active' AS 'Active/Inactive (R)',
     '' AS 'Learning Style'
 
@@ -37,7 +37,7 @@ FROM v_AdHocStudent vahs
     JOIN School sch ON sch.schoolID = vahs.schoolID
 
 WHERE 
-	vahs.endDate IS NULL AND vahs.startStatus <> 'E'
+    vahs.endDate IS NULL AND vahs.startStatus <> 'E'
     --You can use this to query for new students on a daily basis: AND vahs.startDate >= CAST(GETDATE() AS DATE)
     AND vahs.endYear = @EndYear
     AND vahs.studentNumber IS NOT NULL
